3f385b0ddca48059757716321b697255e55df353
Nkemjika1divine <cndivine@gmail.com>
2023-10-31 14:01:45 -0700

    Binary Trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -28,7 +28,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	else
 	{
-		temp = parent;
+		temp = parent->left;
 
 		parent->left = leftnode;
 		leftnode->parent = parent;
