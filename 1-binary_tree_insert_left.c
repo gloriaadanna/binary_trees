b5eff8e817929dd1cc7e78a9c830ebf6052ed680
Nkemjika1divine <cndivine@gmail.com>
2023-10-31 12:50:49 -0700

    Binary Trees

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,40 @@
+#include "binary_tree.h"
+
+/**
+ * binary_tree_insert_left - tgis function inserts anode to te left of a parent
+ *
+ * @parent: the parent node
+ * @value: tge data to store in the node
+ *
+ * Return: the poibter to the created node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *leftnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	binary_tree_t *temp;
+
+	if (leftnode == NULL || parent == NULL)
+		return (NULL);
+
+	leftnode->parent = NULL;
+	leftnode->n = value;
+	leftnode->left = NULL;
+	leftnode->right = NULL;
+
+	if (parent->left == NULL)
+	{
+		leftnode->parent = parent;
+		parent->left = leftnode;
+	}
+	else
+	{
+		temp = parent;
+
+		parent->left = leftnode;
+		leftnode->parent = parent;
+		temp->left = leftnode;
+		leftnode->left = temp;
+	}
+
+	return (leftnode);
+}
