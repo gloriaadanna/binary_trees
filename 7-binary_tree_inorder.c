9ba10d94cc05d961b1d9a43c4b2a0f141fe11f81
gloriaadanna <gloriaagwam7@gmail.com>
2023-11-01 11:50:52 -0700

    task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -6,12 +6,12 @@
  * @tree: A pointer to the root node of the tree
  * @func: A pointer to a function
  */
+
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree && func)
+	if (tree == NULL )
 	{
-		binary_tree_inorder(tree->left, func);
-		func(tree->n);
-		binary_tree_inorder(tree->right, func);
+		binary_tree_inorder(tree->right, NULL);
+		binary_tree_inorder(tree->left, NULL);
 	}
 }
