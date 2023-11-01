a938c114133edc719888d0d1f0c9b7e4d8ecfddb
gloriaadanna <gloriaagwam7@gmail.com>
2023-11-01 11:38:15 -0700

    task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -4,12 +4,13 @@
  * binary_tree_delete - This program Deletes a binary tree.
  * @tree: A pointer to the tree's root node
  */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree != NULL)
+	if (tree == NULL)
 	{
-		binary_tree_delete(tree->left);
 		binary_tree_delete(tree->right);
+		binary_tree_delete(tree->left);
 		free(tree);
 	}
 }
