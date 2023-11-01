05d02d08e84b0b688731dde2824e60649b2f17dc
gloriaadanna <gloriaagwam7@gmail.com>
2023-11-01 14:51:30 -0700

    task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Get tree size
+ * @tree: Root of tree
+ * Return: Size of tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t l = 0, r = 0;
+
+		l = binary_tree_size(tree->left);
+		r = binary_tree_size(tree->right);
+		return (l + r + 1);
+	}
+	return (0);
+}
