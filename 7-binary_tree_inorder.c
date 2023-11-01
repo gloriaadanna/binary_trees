ec88fb8375ab4f79eb9f3634714e15b627191eef
gloriaadanna <gloriaagwam7@gmail.com>
2023-11-01 12:07:17 -0700

    task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -9,9 +9,10 @@
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == func)
+	if (tree == NULL)
 	{
 		binary_tree_inorder(tree->right);
+		func(tree->data);
 		binary_tree_inorder(tree->left);
 	}
 }
